f40aeb5855fe243e0df0e3bed1056641ca17880a
Balaji <balajik.hgi@gmail.com>
2023-09-28 12:29:51 -0400

    metadata updated

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -4,15 +4,16 @@ const layout = () => {
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
-        <Skeleton className=" h-[274px] w-full rounded-lg bg-red-500" />
+        <Skeleton className=" h-[274px] w-full rounded-lg bg-black-200/40" />
       </section>
 
       <section>
         <Skeleton className="mt-6 flex w-full flex-col sm:mt-20" />
+      type: 'resource',
         <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-          <Skeleton className="w-full h-[440px] sm:w-[356px]" />
-          <Skeleton className="w-full h-[440px] sm:w-[356px]" />
-          <Skeleton className="w-full h-[440px] sm:w-[356px]" />
+          <Skeleton className="w-full h-[440px] sm:w-[356px] bg-black-200/40" />
+          <Skeleton className="w-full h-[440px] sm:w-[356px] bg-black-200/40" />
+          <Skeleton className="w-full h-[440px] sm:w-[356px] bg-black-200/40" />
         </div>
       </section>
     </main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,15 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'RMINT Chef Home Page',
   description: 'Create Your Tastiest NFTs',
+  other: {
+    'theme-color': '#0d1117',
+    "color-scheme": "dark only",
+    "twitter:site": "@RMINT_Official",
+    "twitter:image" : "https://ucarecdn.com/55527cd8-0b71-42ef-abc3-90d6955546ac/Screenshot20230911211025.png",
+    "og:url" : "rmint.co",
+    "og:image" : "https://ucarecdn.com/55527cd8-0b71-42ef-abc3-90d6955546ac/Screenshot20230911211025.png",
+    "og:type" : "website",
+  }
 }
 
 export default function RootLayout({
